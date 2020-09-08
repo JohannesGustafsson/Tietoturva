9fc71b1a2c4382f57bebd37c3056ce7952a93ba8
Johannes Gustafsson <70580226+JohannesGustafsson@users.noreply.github.com>
2020-09-08 14:32:40 +0300

    Add files via upload

---
+++ GPOraportti.ps1
@@ -0,0 +1,8 @@
+﻿# Skripti, joka tuottaa HTML-pohjaisen raportin ryhmäkäytännöistä
+
+# Kysytään käyttäjältä tiedoston nimi ja polku
+
+$Tiedosto = Read-Host -Prompt " Anna tiedoston polku ja nimi"
+
+# Luodaan kaikki GPO:t sisätltävä HTML-raportti ja tallennetaan se
+Get-GPOReport -All -ReportType html -Path $Tiedosto
---
+++ KayttajatiedotShell.ps1
@@ -0,0 +1,10 @@
+﻿# Haetaan AD-käyttäjän olennaiset tiedot
+
+# Kysytään käyttäjältä, minkä nimiseen tiedostoon käyttäjät dokumentoidaan
+$Tiedosto = Read-host "mihin tallennetaan, anna polku ja nimi"
+
+# Luetaan AD-käyttäjätiedot muuttujaan
+$Käyttäjät = Get-ADUser -Filter * -Properties AccountExpirationdate, Office, OfficePhone Description, DisplayName, whenCreated, whenChanged
+
+# Putkiteaan tiedot CSV-tiedostoon
+$Käyttäjät | Export-Csv -Encoding Unicode -Delimiter "`t" -Path $Tiedosto
